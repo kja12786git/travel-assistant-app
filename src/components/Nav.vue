47da033b2361011122bcb2aacac9c02e87827852
kjerrya <lifthz@gmail.com>
2018-02-24 18:20:22 -0500

    Added the Nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>{{msg}}</li>
+        <li><a href="/">Search</a></li>
+        <li><a href="SavedList">Saved List</a></li>        
+
+      </ul>
+
+    </nav>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Nav',
+    data () {
+      return {
+        msg: 'To display data for navigation of application.',
+
+      }
+    }
+  }
+
+</script>
