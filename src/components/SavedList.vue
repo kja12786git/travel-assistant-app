cf315a776868637cfeebfa655fab2e2c269373bc
kjerrya <lifthz@gmail.com>
2018-02-26 23:39:03 -0500

    Updated the msg sting

--- src/components/SavedList.vue
+++ src/components/SavedList.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="savedlist">
+    <h1>Saved List</h1>
     <h2>{{msg}}</h2>
 
   </div>
 </template>
 
 <script>
+  import list from '@/assets/dbstatic.js'
+  import stored from '@/assets/stored.js'
+
   export default {
     name: 'SavedList',
     data () {
       return {
-        msg: 'Not found page yet. This will be saved list.'
+        msg: 'You saved the following items of interest...',
+        cities: list,
+        saved: [] //not sure if i'll need to use this yet...
 
       }
 
     }
+
   }
 </script>
