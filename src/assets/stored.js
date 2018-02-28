02aec02378aa0a458200cdcd8288856f08116c7b
kjerrya <lifthz@gmail.com>
2018-02-28 01:14:27 -0500

    Added config for Veux use

--- src/assets/stored.js
+++ src/assets/stored.js
@@ -2,23 +2,54 @@
 //const SOMEKEY = ''; // a key for what?
 import Vue from 'vue'
 import Vuex from 'vuex'
+import data from './modules/savedlist'
 
 Vue.use(Vuex)
 
+const savehere = [data]; // this may need a different key for pois exclusively?
+
+console.log(`${savehere} is data feed...` )
+
 const stored = new Vuex.Store({
-  state: {
-    count: 0
+
+  state: { // keep track of state changes
+    items: savehere,
+    pushto: []
 
   },
-  mutations: {
-    increment (state) {
-      state.count++
+
+  getters: {
+    listCount: state => {
+      return state.pushto.length
 
     }
+
+  },
+
+  mutations: {
+    addToList(state, payload) {
+      state.pushto.push()
+
+    }
+
+  },
+
+  actions: {
+    addToList(context, payload) {
+      context.commit('addToList', payload.item)
+
+    }/*,
+
+    removeFromList() {
+      context.
+
+    }
+*/
   }
+
 })
 
-console.log(stored.state.count)
+console.log(stored.state.pushto.length)
 
 export default {
   stored
