3ff793377675068d00ea3ab53c83ae108bcbf304
kjerrya <lifthz@gmail.com>
2018-02-28 01:21:37 -0500

    This will feed in the saved items once the vuex data for state management error is corrected in stored.js

--- src/components/SavedList.vue
+++ src/components/SavedList.vue
@@ -3,14 +3,22 @@
     <h1>Saved List</h1>
     <h2>{{msg}}</h2>
 
+    <pre>{{ pushto }}</pre>
+
   </div>
 </template>
 
 <script>
+  import { mapState, mapGetters } from 'vuex'
   import list from '@/assets/dbstatic.js'
 
   export default {
     name: 'SavedList',
+    computed: {
+      ...mapState(['pushto']),
+      ...mapGetters(['listCount'])
+
+    },
     data () {
       return {
         msg: 'You saved the following items of interest...',
@@ -21,4 +29,5 @@
     }
 
   }
+
 </script>
