e7c2eaf3421f0a56f9e4868620c15d629a680ba3
kjerrya <lifthz@gmail.com>
2018-02-28 18:37:58 -0500

    Comment removed from file

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <h4>{{msg}}</h4>
-    <em>K. Jerry Alleyne</em>
+    <em><a style="font-size: bold;" target="github" href="https://github.com/kjerrya">@kjerrya</a></em>
 
   </footer>
 </template>
@@ -11,7 +11,7 @@
     name: 'Footer',
     data () {
       return {
-        msg: 'Excellently put together via an \'Vue-cli\' build by...'
+        msg: 'Excellently put together using a \'Vue-cli\' build'
 
       }
     }
@@ -25,7 +25,7 @@
     bottom: 0;
     padding: 1em 0;
     padding: 1rem 0;
-    margin: 2em 0 0 0;    
+    margin: 2em 0 0 0;
     margin: 2rem 0 0 0;
     text-indent: 1rem;
     background-color: #020202;
